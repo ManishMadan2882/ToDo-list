11b1e68aa2a4a1ba0c76226bea49d9eba55f8347
Manish Madan <96079232+ManishMadan2882@users.noreply.github.com>
2022-10-31 23:35:12 +0530

    Add files via upload

--- src/routers/route.js
+++ src/routers/route.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 
+
+const con = process.env;
+
 const express = require('express');
 
 const path = require('path');
@@ -15,6 +18,7 @@ const db = require('../schemadb/scheme');
 const auth  = require('../middleware/auth');
 
 const secret_key =process.env.SECRET_KEY;
+
 router.use(express.json({limit:'100kb'}));
 router.get('/',(req,res) => {
     
@@ -35,20 +39,23 @@ router.get('/api/userdata',auth,async (req,res) => {
     {
         const username = req.user.username;
         const usr = await db.findOne({username : username});
-        res.status(200).send(usr.userdata);  
+       
+        res.status(200).json({
+         userdata : usr.userdata,
+         username : username
+        });  
     }
     catch(err){
      res.status(404).json('not found');
     }
 });
-
+//user data update route
 router.patch('/api/userdata',auth,async (req,res) => {
     try
     {
         const username = req.user.username;
         const li = {userdata :  req.body.userdata};
-        
-        const usr = await db.findOneAndUpdate({username : username},li,{
+       const usr = await db.findOneAndUpdate({username : username},li,{
             new : true
         });
         res.status(200).send(usr);  
@@ -71,52 +78,63 @@ router.get('/register',(req,res) => {
 });
 router.post('/register',async (req,res)=>{
     try{
+        console.log(req.body);
        const username = req.body.username;
 
-       const password =  await bcrypt.hash(req.body.password,4);
+       const pwd1 = await bcrypt.hash(req.body.password, 4);
        
        const newUser = new db({
-           username:username,
-           password: password,
-           
-         })
-         //res.cookie("code",token);
-         const registered = await newUser.save();
-         res.json("newUser");
-         res.redirect('../login');
+           username: username,
+           password : pwd1
+         });
+         const registered = newUser.save((err) => {
+             console.log(err);
+         });
+         res.json(registered);
+       //  res.redirect('../login');
      }
      catch(error){
-       res.status(400).send(error);
+        console.log(error);
+       res.status(400).json(error);
      } 
      
    }
 );
+//login api to authenticate details
 router.post( '/cred' , async (req,res) => {
     try
     {
+
         const user = req.body.username;
 
         const pwd = req.body.password;
         
         const creds = await db.findOne({username:user});
-
-        const authen = bcrypt.compare(pwd,creds.password);
+        
+        const authen = await bcrypt.compare(pwd,creds.password);
 
         if(authen)
         {
-            const token = jwt.sign({username:username},secret_key); 
-            res.json({message : token});
-            res.redirect('../');
+            
+            const token = jwt.sign({username:user},secret_key); 
+      
+            res.cookie("code",token)
+
+            res.status(200).json({message : authen});
+      
         }
         else
         {
-            res.json({ message : "auth failure"})
-            res.redirect('/')
+
+            res.status(403).json({ message : "auth failure"})
+     
         }
     }
     catch(error) {
+        
+        console.log(error);
 
-        res.send('not found');
+        res.status(400).json({message: "not found"})
  
     }
 })
